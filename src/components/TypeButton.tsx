7847b699b57e05c9d163515e8b0f4acb0be7b0b2
williams <williamskusnandi@gmail.com>
2021-10-16 11:03:55 +1100

    feat: type images

--- src/components/TypeButton.tsx
+++ src/components/TypeButton.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from "react";
-import { TouchableOpacity } from "react-native";
-import styled from "styled-components/native";
+import { ImageSourcePropType, TouchableOpacity } from "react-native";
 import { Text } from "react-native";
+import styled from "styled-components/native";
 
 interface Props {
   onPress: () => void;
   selected: boolean;
   title: string;
   description: string;
+  imgSource: ImageSourcePropType;
 }
 
 export const TypeButton: FC<Props> = (props) => {
-  const { onPress, title, description, selected } = props;
+  const { onPress, title, description, selected, imgSource } = props;
   return (
     <TouchableOpacity onPress={onPress}>
       <Container selected={selected}>
@@ -23,27 +24,26 @@ export const TypeButton: FC<Props> = (props) => {
             <Text>{description}</Text>
           </TypeDescription>
         </TextContainer>
-        <TypePicture></TypePicture>
+        <TypeImage source={imgSource} />
       </Container>
     </TouchableOpacity>
   );
 };
 
 const Container = styled.View<{ selected: boolean }>`
-  background:${(p) =>
-    p.selected ? p.theme.color.background : p.theme.color.foreground}
+  background: ${(p) =>
+    p.selected ? p.theme.color.background : p.theme.color.foreground};
   height: 73px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   padding: 6px 8px 10px 7px;
-  
 `;
 
-const TypePicture = styled.View`
-  background: grey;
+const TypeImage = styled.Image`
   width: 65px;
-  height: 60px;
+  height: 65px;
+  resize-mode: contain;
 `;
 
 const TextContainer = styled.View`
--- src/components/TypeSelect.tsx
+++ src/components/TypeSelect.tsx
@@ -1,29 +1,56 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import styled from "styled-components/native";
 
 import { Card } from "./Card";
 import { TypeButton } from "./TypeButton";
-import styled from "styled-components/native";
 
 interface Props {
   type: boolean[];
   onTypeSelect: (type: boolean[]) => void;
 }
 
+/**
+ * Poop types information based on Bristol Stool Chart
+ *
+ * @see https://vignette.wikia.nocookie.net/house/images/2/21/Bristol_stool_chart.svg/revision/latest?cb=20110823023517
+ */
 export const typeInfoArr = [
-  { title: "Type 1", description: "Small, hard, separate lumps" },
-  { title: "Type  2", description: "Sausage-shaped and lumpy" },
+  {
+    title: "Type 1",
+    description: "Small, hard, separate lumps",
+    imgSource: require("../../assets/types/type-1.png"),
+  },
+  {
+    title: "Type  2",
+    description: "Sausage-shaped and lumpy",
+    imgSource: require("../../assets/types/type-2.png"),
+  },
   {
     title: "Type  3",
     description: "Sausage-shaped with cracks over the surface",
+    imgSource: require("../../assets/types/type-3.png"),
   },
-  { title: "Type  4", description: "Smooth and soft, Sausage-shaped, " },
-  { title: "Type  5", description: "Small and soft blobs with defined edges" },
+  {
+    title: "Type  4",
+    description: "Smooth and soft, Sausage-shaped",
+    imgSource: require("../../assets/types/type-4.png"),
+  },
+  {
+    title: "Type  5",
+    description: "Small and soft blobs with defined edges",
+    imgSource: require("../../assets/types/type-5.png"),
+  },
   {
     title: "Type  6",
     description: "Fluffy/mushy pieces with ragged edges",
+    imgSource: require("../../assets/types/type-6.png"),
   },
-  { title: "Type  7", description: "Watery consistency with no solid pieces" },
+  {
+    title: "Type  7",
+    description: "Watery consistency with no solid pieces",
+    imgSource: require("../../assets/types/type-7.png"),
+  },
 ];
 
 export const TypeSelect = (props: Props) => {
